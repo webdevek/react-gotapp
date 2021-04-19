dbbfcbec36ddd940444ecf226399bee69a6b97c6
webdevek <webdevek@webdevek.ru>
2021-04-19 16:25:35 +0300

    commit add info gotServices.js and randomChar.js

--- src/services/gotServices.js
+++ src/services/gotServices.js
@@ -11,10 +11,51 @@ export default class GotServices {
         }
         return await res.json();
     }
-    getAllCharacters() {
-        return this.getResource(`/characters?page=5&pageSize=10`);
+    async getAllCharacters() {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
+        return res.map(this._transformCharacter);
     }
-    getCharacter(id) {
-        return this.getResource(`/characters/${id}`);
+    async getCharacter(id) {
+        const character = await this.getResource(`/characters/${id}`);
+        return this._transformCharacter(character);
+    }
+    getAllHouses() {
+        return this.getResource(`/houses`);
+    }
+    getHouses(id) {
+        return this.getResource(`/houses/${id}`);
+    }
+    getAllBooks() {
+        return this.getResource(`/books`);
+    }
+    getBooks(id) {
+        return this.getResource(`/books/${id}`);
+    }
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+    }
+    _transformHouse(house) {
+        return {
+            name: house.name,
+            region: house.region,
+            words: house.words,
+            title: house.title,
+            overlord: house.overlord,
+            ancestralWeapons: house.ancestralWeapons
+        }
+    }
+    _transformBook(book) {
+        return {
+            name: book.name,
+            numberOfPages: book.numberOfPages,
+            publiser: book.publiser,
+            released: book.released
+        }
     }
 }
